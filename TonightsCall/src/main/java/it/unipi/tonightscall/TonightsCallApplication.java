ddb6dd8f3bd330087aa7df8d9469ec138d34c831
franc <francescodm200218@gmail.com>
2026-01-21 20:15:54 +0100

    Started Java Project

---
+++ TonightsCall/src/main/java/it/unipi/tonightscall/TonightsCallApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.tonightscall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonightsCallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TonightsCallApplication.class, args);
+    }
+
+}
---
+++ TonightsCall/src/test/java/it/unipi/tonightscall/TonightsCallApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.tonightscall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonightsCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
