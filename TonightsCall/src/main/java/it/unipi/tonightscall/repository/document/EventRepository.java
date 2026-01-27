ffd600de60bdf1ac69ed3197c0b3c79f822e27a8
franc <francescodm200218@gmail.com>
2026-01-28 00:48:24 +0100

    Modify Event controller so the response are always ResponseEntity. Also, I Added the swagger notation
    TODO:
    -Modify EventController.updateEvent to acept also Organiation
    -Write Input Control
    -Refactor JWTService and AuthService adding adHoc Exception
    -Refactor Mapper. This work but is ugly.

--- TonightsCall/src/main/java/it/unipi/tonightscall/repository/document/EventRepository.java
+++ TonightsCall/src/main/java/it/unipi/tonightscall/repository/document/EventRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.geo.Point;
 
-import java.awt.*;
 import java.time.LocalDate;
 import java.util.List;
 
