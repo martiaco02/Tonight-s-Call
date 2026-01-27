f6a6b672508cac4dec47badc0c876b989e5245a5
Alessandro Xavier Battisti <AlexanderBattisti@gmail.com>
2026-01-27 21:35:23 +0100

    Add "updateEvent" to EventController and EventService Add to Mapper: -mapEventToDto -mapLocationToEntity -mapLocationToDto -mapAddressToEntity -mapAddressToDto -mapStatisticsToDto

--- TonightsCall/src/main/java/it/unipi/tonightscall/controller/EventController.java
+++ TonightsCall/src/main/java/it/unipi/tonightscall/controller/EventController.java
@@ -1,5 +1,11 @@
 package it.unipi.tonightscall.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import it.unipi.tonightscall.DTO.EventDTO;
 import it.unipi.tonightscall.entity.document.Event;
 import it.unipi.tonightscall.service.EventService;
 import jakarta.validation.constraints.Min;
@@ -8,6 +14,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.geo.Distance;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
@@ -25,41 +33,112 @@ public class EventController {
         this.eventService = eventService;
     }
 
+    /**
+     * Find every event
+     *
+     * @param page  The number of pages to return (the result is paginated)
+     */
     @GetMapping
     public Page<@NonNull Event> getAllEvents(@RequestParam(defaultValue = "0") @Min(0) int page) {
         Pageable pageable = PageRequest.of(page, this.eventService.PAGE_SIZE);
         return this.eventService.getAllEvents(pageable);
     }
 
-    //  Find events by id
+    /**
+     * Find an event with a certain id
+     *
+     * @param id  The id of the event
+     */
     @GetMapping("/{id}")
     public Optional<Event> getEventById(@PathVariable String id) { return this.eventService.getEventById(id); }
 
-    //  Find events by topic list
+    /**
+     * Find events based on their topics
+     *
+     * @param topics  The topics of the event
+     * @param page  The number of pages to return (the result is paginated)
+     */
     @GetMapping("/topic")
     public Page<@NonNull Event> getEventsByTopic(@RequestParam List<String> topics, @RequestParam(defaultValue = "0") @Min(0) int page) {
         Pageable pageable = PageRequest.of(page, this.eventService.PAGE_SIZE);
         return this.eventService.getEventsByTopic(topics, pageable);
     }
 
-    //  Find events by starting date
+    /**
+     * Find events based on their starting date
+     *
+     * @param startingDate  The starting date of the event
+     * @param page  The number of pages to return (the result is paginated)
+     */
     @GetMapping("/date")
     public Page<@NonNull Event> getEventsByDate(@RequestParam LocalDate startingDate, @RequestParam(defaultValue = "0") @Min(0) int page) {
         Pageable pageable = PageRequest.of(page, this.eventService.PAGE_SIZE);
         return this.eventService.getEventsByDate(startingDate, pageable);
     }
 
-    //  Find events by location
+    /**
+     * Find events based on their location
+     *
+     * @param location  The center point around which to look for
+     * @param distance  The maximum admissible distance from "location"
+     * @param page  The number of pages to return (the result is paginated)
+     */
     @GetMapping("/location")
     public Page<@NonNull Event> getEventsByLocation(@RequestParam Point location, Distance distance, @RequestParam(defaultValue = "0") @Min(0) int page) {
         Pageable pageable = PageRequest.of(page, this.eventService.PAGE_SIZE);
         return this.eventService.getEventsByLocation(location, distance, pageable);
     }
 
-    //  Find events by topic list and starting date
+    /**
+     * Find events based on their topics and starting date
+     *
+     * @param categories  The topics of the events
+     * @param date  The starting date of the events
+     * @param page  The number of pages to return (the result is paginated)
+     */
     @GetMapping("/topic-date")
     public Page<@NonNull Event> getEventsByTopicAndDate(@RequestParam List<String> categories, @RequestParam LocalDate date, @RequestParam(defaultValue = "0") @Min(0) int page) {
         Pageable pageable = PageRequest.of(page, this.eventService.PAGE_SIZE);
         return this.eventService.getEventsByTopicAndDate(categories, date, pageable);
+    }
+
+    /**
+     * Updates the data of an event
+     *
+     * @param event_id  The ID of the updated event
+     * @param authentication  The security context containing the current user's details (injected by Spring Security).
+     * @param eventDTO The updated details of the event
+     * @return The updated eventDTO and the list of emails of every user which stated their presence at the event if successful, or an error message otherwise.
+     */
+    @Operation(
+            summary = "Updates an Event's data",
+            description = "Updates information about an existing Event."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Event updated successfully",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = EventDTO.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid input, Event not found",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden (User is not authorized)",
+                    content = @Content(mediaType = "text/plain")
+            )
+    })
+    @PutMapping("/update-event/{event_id}")
+    public ResponseEntity<?> updateEvent(@PathVariable String event_id, Authentication authentication, @RequestBody EventDTO eventDTO) {
+        try{
+            EventDTO updatedEvent = eventService.updateEvent(event_id, authentication.getName(), eventDTO);
+            return ResponseEntity.ok(updatedEvent);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 }
--- TonightsCall/src/main/java/it/unipi/tonightscall/service/EventService.java
+++ TonightsCall/src/main/java/it/unipi/tonightscall/service/EventService.java
@@ -1,13 +1,19 @@
 package it.unipi.tonightscall.service;
 
+import it.unipi.tonightscall.DTO.EventDTO;
 import it.unipi.tonightscall.entity.document.Event;
+import it.unipi.tonightscall.entity.document.EventOrganization;
+import it.unipi.tonightscall.entity.document.Organizer;
+import it.unipi.tonightscall.utilies.Mapper;
 import it.unipi.tonightscall.repository.document.EventRepository;
+import it.unipi.tonightscall.repository.document.OrganizerRepository;
 import it.unipi.tonightscall.repository.graph.EventGraphRepository;
 import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.geo.Distance;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.awt.*;
 import java.time.LocalDate;
@@ -20,11 +26,14 @@ public class EventService {
     public final EventRepository eventRepository;
     public final EventGraphRepository eventGraphRepository;
 
+    public final OrganizerRepository organizerRepository;
+
     public final int PAGE_SIZE = 10;
 
-    public EventService(EventRepository eventRepository, EventGraphRepository eventGraphRepository) {
+    public EventService(EventRepository eventRepository, EventGraphRepository eventGraphRepository, OrganizerRepository organizerRepository) {
         this.eventRepository = eventRepository;
         this.eventGraphRepository = eventGraphRepository;
+        this.organizerRepository = organizerRepository;
     }
 
     /**
@@ -75,5 +84,116 @@ public class EventService {
      */
     public Page<@NonNull Event> getEventsByTopicAndDate(List<String> categories, LocalDate date, Pageable pageable) {
         return this.eventRepository.findByCategoriesInAndStartingDateGreaterThanEqual(categories, date, pageable);
+    }
+
+    /**
+     * Updates an Event's data
+     * <p>
+     * This method:
+     * <ol>
+     *      <li>Checks if the Event exists.</li>
+     *      <li>Checks if the Organizer who requested the Event update is the owner of the Event</li>
+     *      <li>Checks for consistency: ??? can't be changed</li>
+     *      <li>Updates the specific data for the Event</li>
+     *      <li>Saves the updated Event to MongoDB.</li>
+     *      <li>Updates the corresponding node in the Graph Database (Neo4j).</li>
+     * </ol>
+     * </p>
+     *
+     * @param event_id The id of the Event that will be updated.
+     * @param username The username of the user who requested the update.
+     * @param newEventDTO The EventDTO with the updated data.
+     * @return The updated EventDTO.
+     * @throws RuntimeException If the event is not found, if the user isn't the organizer of the event or if illegal fields are updated
+     */
+
+    @Transactional
+    public EventDTO updateEvent(String event_id, String username, EventDTO newEventDTO){
+        Organizer organizer = organizerRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Organizer Not Found!"));
+
+        //  checking if the event exists
+        Event oldEvent = eventRepository.findById(event_id)
+                .orElseThrow(() -> new RuntimeException("Event Not Found!"));
+
+        // checking consistency: event id can't change
+        if(newEventDTO.getId() != null && !oldEvent.getId().equals(newEventDTO.getId())){
+            throw new RuntimeException("Can't change event ID!");
+        }
+
+        // checking consistency: event name can't change
+        if(newEventDTO.getEventName() != null && !oldEvent.getEventName().equals(newEventDTO.getEventName())){
+            throw new RuntimeException("Can't change event name!");
+        }
+
+        // checking consistency: event reviews can't change
+        if(newEventDTO.getReviews() != null) {
+            throw new RuntimeException("Can't change event reviews!");
+        }
+
+        // checking consistency: event attendees can't change
+        if(newEventDTO.getAttendees() != null){
+            throw new RuntimeException("Can't change event attendees!");
+        }
+
+        // checking consistency: event statistics can't change
+        if(newEventDTO.getStatistic() != null){
+            throw new RuntimeException("Can't change the statistics of the event!");
+        }
+
+        // checking if the user is the organizer of the event
+        List<EventOrganization> organizer_events = organizer.getEvents();
+
+        for(EventOrganization e : organizer_events) {
+            if(e.getId().equals(event_id)) { break; }
+            throw new RuntimeException("The user is not the organizer of this event!");
+        }
+
+        // updating the new data
+
+        //  Location
+        if(newEventDTO.getPosition() != null){
+            oldEvent.setPosition(Mapper.mapAddressToEntity(newEventDTO.getPosition()));
+        }
+
+        //  Ticket price
+        if(newEventDTO.getTicketPrice() != null){
+            oldEvent.setTicketPrice(newEventDTO.getTicketPrice());
+        }
+
+        //  Starting date
+        if(newEventDTO.getStartingDate() != null){
+            oldEvent.setStartingDate(newEventDTO.getStartingDate());
+        }
+
+        //  Ending date
+        if(newEventDTO.getEndingDate() != null){
+            oldEvent.setEndingDate(newEventDTO.getEndingDate());
+        }
+
+        //  Topics
+        if(newEventDTO.getCategories() != null){
+            oldEvent.setCategories(newEventDTO.getCategories());
+        }
+
+        //  Description
+        if(newEventDTO.getDescription() != null){
+            oldEvent.setDescription(newEventDTO.getDescription());
+        }
+
+        //  Poster
+        if(newEventDTO.getUrlImg() != null){
+            oldEvent.setUrlImg(newEventDTO.getUrlImg());
+        }
+
+        //  Starting times
+        if(newEventDTO.getStartingTimes() != null){
+            oldEvent.setStartingTimes(newEventDTO.getStartingTimes());
+        }
+
+        // updating document
+        eventRepository.save(oldEvent);
+
+        return Mapper.mapEventToDto(oldEvent);
     }
 }
